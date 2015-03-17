8501f68038a526673fb13c7dc4a048f3e72f4453
Matthew Villwock <mtvillwock@gmail.com>
2015-03-17 01:42:56 -0700

    modify create route to render form again if entry is invalid

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     # @article = Article.new(params.require(:article).permit(:title, :text)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def update
