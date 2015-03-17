8ca240b65396ca1150aa747e8557ed6ef8a05964
Matthew Villwock <mtvillwock@gmail.com>
2015-03-17 02:43:44 -0700

    add delete route and link to index file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,10 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
+    redirect_to articles_path
   end
 
   private
