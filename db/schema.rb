36b9f6d9e878b900d6ec9edd89ee8a0bba81ed8a
Matthew Villwock <mtvillwock@gmail.com>
2015-03-18 01:28:42 -0700

    added nav/footer, revised heading, created migration for article summaries

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317095202) do
+ActiveRecord::Schema.define(version: 20150318082753) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "summary"
   end
 
   create_table "comments", force: :cascade do |t|
