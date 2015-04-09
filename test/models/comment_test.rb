c5301a3d2e92987bb8b15ffc82128bf2b55fcdc0
Matthew Villwock <mtvillwock@gmail.com>
2015-04-08 21:26:18 -0700

    add RSPEC test skeletons

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,24 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  describe 'valid comment' do
+    let!(:comment) # double check syntax on this
+    it 'should ' do
+
+    end
+
+    it 'should ' do
+
+    end
+  end
+
+  describe 'invalid comment' do
+    it 'should ' do
+
+    end
+
+    it 'should ' do
+
+    end
+  end
 end
