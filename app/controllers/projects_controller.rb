1c068d75ceb6206a11cd1e077376077546129a63
Matthew Villwock <mtvillwock@gmail.com>
2015-06-04 16:05:49 -0700

    implement projects partial and index and show pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,27 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find_by(params[:title])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save!
+      redirect_to '/projects'
+    end
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name,
+                                      :month,
+                                      :tagline,
+                                      :description,
+                                      :summary,
+                                      :github_url,
+                                      :img_src,
+                                      :deploy_url)
+    end
 end
